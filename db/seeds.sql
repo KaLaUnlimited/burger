90939a9089840f1a839cc29af0f909d94fff58b4
kalaunlimited <KLasode@gmail.com>
2018-03-08 01:45:12 -0500

    db setup files completed, may need mysql command line client

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO schools (burger_name,devoured) VALUES ("Cheeseburger", False);
+INSERT INTO schools (burger_name,devoured)VALUES ("lambburger", true);
+INSERT INTO schools (burger_name,devoured) VALUES ("TurkeyBurger", false);
+INSERT INTO schools (burger_name,devoured) VALUES ("veggie", false);
