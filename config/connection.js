092da1e353ca1dadd0430aada2f13a13bb6beab4
kalaunlimited <KLasode@gmail.com>
2018-03-12 15:45:11 -0400

    commented out port number

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ var connection;
   connection=mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   connection=mysql.createConnection({
-  port: 3306,
+  // port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
